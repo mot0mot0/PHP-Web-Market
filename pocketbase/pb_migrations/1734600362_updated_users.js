e08dd02c524b4c74f32e3584495c6c0b9807081e
moto_moto <ustinovilia34@gmail.com>
2024-12-19 12:32:25 +0300

    feat: updated db structure

---
+++ pocketbase/pb_migrations/1734600362_updated_users.js
@@ -0,0 +1,73 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": ""
+      }
+    }
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("file376926767")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "select1542800728",
+    "maxSelect": 1,
+    "name": "field",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "admin",
+      "seller",
+      "buyer"
+    ]
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": "avatar"
+      }
+    }
+  }, collection)
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "file376926767",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "avatar",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": null,
+    "type": "file"
+  }))
+
+  // remove field
+  collection.fields.removeById("select1542800728")
+
+  return app.save(collection)
+})
