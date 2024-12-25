dd7d81f6e2dbf6d4951eb8bd15b4fc190134f1a3
moto_moto <ustinovilia34@gmail.com>
2024-12-26 00:02:36 +0300

    fix: fixed adminPage header styles

--- app/views/adminPage.php
+++ app/views/adminPage.php
@@ -11,36 +11,38 @@
     <?php require_once __DIR__ . '/partials/navbar.php'; ?>
 
     <div class="admin-container">
-        <h3 class="section-title">Добавление товара</h3>
+        <h1 class="page-title">Добавление товара</h1>
+        
+        <div class="orders-adding-card">
+            <?php 
+                if (isset($_SESSION['error'])) {
+                    $message = $_SESSION['error'];
+                    echo "<script>alert('$message');</script>";
+                    unset($_SESSION['error']);
+                };
+                if (isset($_SESSION['success'])) {
+                    $message = $_SESSION['success'];
+                    echo "<script>alert('$message');</script>";
+                    unset($_SESSION['success']);
+                }
+            ?>
 
-        <?php 
-            if (isset($_SESSION['error'])) {
-                $message = $_SESSION['error'];
-                echo "<script>alert('$message');</script>";
-                unset($_SESSION['error']);
-            };
-            if (isset($_SESSION['success'])) {
-                $message = $_SESSION['success'];
-                echo "<script>alert('$message');</script>";
-                unset($_SESSION['success']);
-            }
-        ?>
+            <form action="/adminPage/addProduct" method="POST" enctype="multipart/form-data" class="admin-form">
+                <label for="name">Название товара</label>
+                <input type="text" name="name" required><br>
 
-        <form action="/adminPage/addProduct" method="POST" enctype="multipart/form-data" class="admin-form">
-            <label for="name">Название товара</label>
-            <input type="text" name="name" required><br>
+                <label for="price">Цена</label>
+                <input type="number" name="price" required><br>
 
-            <label for="price">Цена</label>
-            <input type="number" name="price" required><br>
+                <label for="description">Описание</label>
+                <textarea name="description" required></textarea><br>
 
-            <label for="description">Описание</label>
-            <textarea name="description" required></textarea><br>
+                <label for="picture">Изображение товара</label>
+                <input type="file" name="picture" required><br>
 
-            <label for="picture">Изображение товара</label>
-            <input type="file" name="picture" required><br>
-
-            <button type="submit" class="button">Загрузить</button>
-        </form>
+                <button type="submit" class="button">Загрузить</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
